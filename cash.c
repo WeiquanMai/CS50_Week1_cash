565ec21e9ba1663df8ae95db93641bfe48e7a5fe
WeiquanMai <weiquanmai@gmail.com>
2025-05-24 16:42:54 -0400

    Update cash.c

--- cash.c
+++ cash.c
@@ -1,3 +1,10 @@
+/*Problem to Solve
+* Suppose you work at a store and a customer gives you $1.00 (100 cents) for candy that costs $0.50 (50 cents).
+* You'll need to pay them their "change", the amount leftover after paying for the cost of the candy.
+* When making change, odds are you want to minimize the number of coins you're dispensing for each customer,
+* lest you run out (or annoy the customer!).
+* In a file called cash.c, implement a program in C that prints ouf the minimum coins needed to make the given amount of change, in cents.
+*/
 #include <cs50.h>
 #include <stdio.h>
 
